586c6aead342de711946a58998dfbc50923f9d90
Diego Gomes <diegogomes@id.uff.br>
2016-07-17 19:21:39 -0300

    Level 1.3

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-var app = angular.module( 'gemStore', [] );
+// Wrapping your Javascript in a clousure is a good habit!
+( function() {
+	var app = angular.module( 'gemStore', [] );
+	
+	//Noteci that controller is attached to ( inside )our app.
+	app.controller( 'StoreController', function() {
+		this.product = gem;
+	});
+
+	var gem = { 
+		name: 'Dodecahedron',
+		price: 2.95,
+		description: '. . .',
+	}
+
+})();
+
+
